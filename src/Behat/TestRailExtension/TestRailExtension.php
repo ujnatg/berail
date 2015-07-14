e52fd4d048d3e1f6f8df13b2d56c0ceebe26fffc
Eugen Degtiarov <ujnatg@gmail.com>
2015-07-14 21:24:36 +0300

    yuri111

--- src/Behat/TestRailExtension/TestRailExtension.php
+++ src/Behat/TestRailExtension/TestRailExtension.php
@@ -12,9 +12,10 @@ use Behat\Testwork\ServiceContainer\Extension;
 use Behat\Testwork\ServiceContainer\ExtensionManager;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 
-class TestRailExtension implements Extension
+class TestRailExtension implements ExtensionInterface
 {
 
     /**
@@ -74,9 +75,57 @@ class TestRailExtension implements Extension
      * @param ContainerBuilder $container
      * @param array $config
      */
-    public function load(ContainerBuilder $container, array $config)
+
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array $config An array of configuration values
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     *
+     * @api
+     */
+    public function load(array $config, ContainerBuilder $container)
     {
         // TODO: Implement load() method.
-        echo "load";
+    }
+
+    /**
+     * Returns the namespace to be used for this extension (XML namespace).
+     *
+     * @return string The XML namespace
+     *
+     * @api
+     */
+    public function getNamespace()
+    {
+        // TODO: Implement getNamespace() method.
+    }
+
+    /**
+     * Returns the base path for the XSD files.
+     *
+     * @return string The XSD base path
+     *
+     * @api
+     */
+    public function getXsdValidationBasePath()
+    {
+        // TODO: Implement getXsdValidationBasePath() method.
+    }
+
+    /**
+     * Returns the recommended alias to use in XML.
+     *
+     * This alias is also the mandatory prefix to use when using YAML.
+     *
+     * @return string The alias
+     *
+     * @api
+     */
+    public function getAlias()
+    {
+        // TODO: Implement getAlias() method.
     }
 }
